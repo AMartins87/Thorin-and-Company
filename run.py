597460901a0337257b73aef6c790e8cd8ccb143d
AMartins87 <aneta.martins@outlook.com>
2022-01-12 21:00:58 +0000

    Add about and contact pages

--- run.py
+++ run.py
@@ -32,6 +32,15 @@ the index function underneath and returns
 the "Hello, World" text.
 """
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def about():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
