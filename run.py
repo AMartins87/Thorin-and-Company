43dd3d8a1ec186bd4c25a8630c9e0208df0ef543
AMartins87 <aneta.martins@outlook.com>
2022-01-17 20:28:03 +0000

    Add member page

--- run.py
+++ run.py
@@ -2,12 +2,11 @@ import os
 import json
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
-
-
 def index():
     return render_template("index.html")
 
@@ -18,6 +17,17 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
@@ -33,15 +43,5 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
-
-"""
-We should never have "debug=True" in a production application,
-or when we submit our projects for assessment.
-This is very important, because having debug=True
-can allow arbitrary code to be run, this is a security flaw.
-You should only have debug=True while testing your
-application in development mode, but change it to
-debug=False before you submit your project
-"""
